89c4e1744e3a12c235a06b6461c229306289935e
Robert McCurdy <rwmccurdy@hotmail.com>
2021-04-30 18:20:43 -0700

    Updating interface with colour

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -2,9 +2,17 @@ import './styles/styles.css';
 
 function App() {
   return (
-    <div className="App">
-      Hello World!
-    </div>
+    <>
+      <div className="portfolio-background">
+        <div className="content-container">
+            Hello World!
+        </div>
+      </div>
+
+      <div className="portfolio-footer-container">
+        <footer> <small>Copyright &copy; 2021 | Robert McCurdy | All Rights Reserved</small> </footer> 
+      </div>
+    </>
   );
 }
 
