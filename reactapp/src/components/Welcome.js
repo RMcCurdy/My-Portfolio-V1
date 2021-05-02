b44923435d40905e6d1bd168aa8f05846712c5b1
Robert McCurdy <rwmccurdy@hotmail.com>
2021-05-02 13:39:58 -0700

    Adding styles for welcome message

--- reactapp/src/components/Welcome.js
+++ reactapp/src/components/Welcome.js
@@ -3,8 +3,12 @@ import React from 'react'
 const Welcome = () => {
   return (
     <div className="content-container">
-      <div className="welcome-content content-height">
-        Welcome
+      <div className="welcome-content welcome-content-height welcome-font">
+        <div className="fade-in">
+          Hi, I am Robert.<br/>
+          University Student and<br/>
+          aspiring Web Developer.
+        </div>
       </div>
     </div>
   )
