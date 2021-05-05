d3c80961a1a65b118b1a8df43d903ec1e5fcb209
Robert McCurdy <rwmccurdy@hotmail.com>
2021-05-04 22:51:35 -0700

    updates

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -5,28 +5,38 @@ import Projects from './components/Projects.js';
 import Skills from './components/Skills.js';
 import About from './components/About.js';
 import Contact from './components/Contact.js';
-import HamburgerMenu from './components/HamburgerMenu';
+// import HamburgerMenu from './components/HamburgerMenu';
 
 function App() {
-  return (
-    <>
-      <div className="portfolio-background">
-        <div>
-          <img className="portfolio-logo-absolute logo-fade-in" src={Logo} alt="RM logo"/>
-          {/* <Sidebar/> */}
-        </div>
-        <Welcome/>
-        <Projects/>
-        <Skills/>
-        <About/>
-        <Contact/>
-      </div>
+    return (
+        <>
+            <div className='portfolio-background'>
+                <div>
+                    <img
+                        className='portfolio-logo-absolute logo-fade-in'
+                        src={Logo}
+                        alt='RM logo'
+                    />
+                    {/* <Sidebar/> */}
+                </div>
+                <Welcome />
+                <Projects />
+                <Skills />
+                <About />
+                <Contact />
+            </div>
 
-      <div className="portfolio-footer-container">
-        <footer> <small>Copyright &copy; 2021 | Robert McCurdy | All Rights Reserved</small> </footer> 
-      </div>
-    </>
-  );
+            <div className='portfolio-footer-container'>
+                <footer>
+                    {' '}
+                    <small>
+                        Copyright &copy; 2021 | Robert McCurdy | All Rights
+                        Reserved
+                    </small>{' '}
+                </footer>
+            </div>
+        </>
+    );
 }
 
 export default App;
--- reactapp/src/components/Welcome.js
+++ reactapp/src/components/Welcome.js
@@ -1,22 +1,29 @@
 import React from 'react';
 import PixelArt from '../img/pxArtShadow2.png';
+import '../styles/styles.css';
 
 const Welcome = () => {
-  return (
-    <div className="content-container">
-      <div className="logo-loading-container">
-        {/* <img className="logo-loading-breathing logo-loading" src={Logo} alt="RM loading"/> */}
-      </div>
-      <div className="welcome-content welcome-content-height welcome-font">
-        <div className="welcome-words fade-in-right-to-left">
-          Hi, I'm Robert.<br/>
-          University Student and<br/>
-          aspiring Web Developer.
+    return (
+        <div className='content-container'>
+            <div className='logo-loading-container'>
+                {/* <img className="logo-loading-breathing logo-loading" src={Logo} alt="RM loading"/> */}
+            </div>
+            <div className='welcome-content welcome-content-height welcome-font'>
+                <div className='welcome-words fade-in-right-to-left'>
+                    Hi, I'm Robert.
+                    <br />
+                    University Student and
+                    <br />
+                    aspiring Web Developer.
+                </div>
+                <img
+                    className='welcome-image fade-in-left-to-right'
+                    src={PixelArt}
+                    alt='My Pixel Art'
+                />
+            </div>
         </div>
-        <img className="welcome-image fade-in-left-to-right" src={PixelArt} alt="My Pixel Art"/>
-      </div>
-    </div>
-  )
-}
+    );
+};
 
-export default Welcome
+export default Welcome;
