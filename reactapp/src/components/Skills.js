6410703e33ff2c42473f63b01c844cede46fde30
Robert McCurdy <rwmccurdy@hotmail.com>
2021-06-16 21:45:28 -0700

    Added P tags for skills

--- reactapp/src/components/Skills.js
+++ reactapp/src/components/Skills.js
@@ -3,10 +3,10 @@ const Skills = () => {
         <div className='content-container-not-welcome'>
             <div className='skills-content content-height'>
                 <div className='content-html-tag-font'>&lt;skills&gt;</div>
-                React.js
-                JavaScript
-                HTML
-                CSS
+                <p>React.js</p>
+                <p>JavaScript</p>
+                <p>HTML</p>
+                <p>CSS</p>
                 <div className='content-html-tag-font'>&lt;/skills&gt;</div>
             </div>
         </div>
