c4858e7bf40d1fdd6ed5f22547834a4a192192ff
Robert McCurdy <rwmccurdy@hotmail.com>
2021-08-30 11:08:22 -0600

    Sidebar with Icons

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -5,12 +5,16 @@ import Projects from './components/Projects.js';
 import Skills from './components/Skills.js';
 import About from './components/About.js';
 import Contact from './components/Contact.js';
+import Sidebar from './components/Sidebar.js';
 // import HamburgerMenu from './components/HamburgerMenu';
 
 function App() {
     return (
         <>
-            <div className='portfolio-background'>
+            <Sidebar />
+            <div
+                className='portfolio-background'
+                style={{ paddingLeft: '4rem' }}>
                 <div>
                     <img
                         className='portfolio-logo-absolute logo-fade-in'
---
+++ reactapp/src/components/Sidebar.js
@@ -0,0 +1,102 @@
+import React from 'react';
+import SideNav, {
+    Toggle,
+    Nav,
+    NavItem,
+    NavIcon,
+    NavText,
+} from '@trendmicro/react-sidenav';
+
+// Icon imports
+import HomeIcon from '@material-ui/icons/Home';
+import LaptopChromebookIcon from '@material-ui/icons/LaptopChromebook';
+import MenuBookIcon from '@material-ui/icons/MenuBook';
+import InfoIcon from '@material-ui/icons/Info';
+import PersonIcon from '@material-ui/icons/Person';
+
+// Be sure to include styles at some point, probably during your bootstraping
+import '@trendmicro/react-sidenav/dist/react-sidenav.css';
+
+const Sidebar = () => {
+    return (
+        <SideNav
+            style={{
+                position: 'fixed',
+                top: '0',
+                left: '0',
+                background: 'hsl(13, 89%, 60%)',
+            }}
+            onSelect={(selected) => {
+                // Add your code here
+            }}>
+            <SideNav.Toggle />
+            <SideNav.Nav defaultSelected='home'>
+                <NavItem eventKey='home'>
+                    <NavIcon
+                        style={{
+                            display: 'flex',
+                            alignItems: 'center',
+                            justifyContent: 'center',
+                        }}>
+                        <HomeIcon />
+                    </NavIcon>
+                    <NavText style={{ fontFamily: 'Montserrat' }}>Home</NavText>
+                </NavItem>
+                <NavItem eventKey='projects'>
+                    <NavIcon
+                        style={{
+                            display: 'flex',
+                            alignItems: 'center',
+                            justifyContent: 'center',
+                        }}>
+                        <LaptopChromebookIcon />
+                    </NavIcon>
+                    <NavText style={{ fontFamily: 'Montserrat' }}>
+                        Projects
+                    </NavText>
+                </NavItem>
+                <NavItem eventKey='skills'>
+                    <NavIcon
+                        style={{
+                            display: 'flex',
+                            alignItems: 'center',
+                            justifyContent: 'center',
+                        }}>
+                        <MenuBookIcon />
+                    </NavIcon>
+                    <NavText style={{ fontFamily: 'Montserrat' }}>
+                        Skills
+                    </NavText>
+                </NavItem>
+                <NavItem eventKey='about'>
+                    <NavIcon
+                        style={{
+                            display: 'flex',
+                            alignItems: 'center',
+                            justifyContent: 'center',
+                        }}>
+                        <InfoIcon />
+                    </NavIcon>
+                    <NavText style={{ fontFamily: 'Montserrat' }}>
+                        About
+                    </NavText>
+                </NavItem>
+                <NavItem eventKey='contact'>
+                    <NavIcon
+                        style={{
+                            display: 'flex',
+                            alignItems: 'center',
+                            justifyContent: 'center',
+                        }}>
+                        <PersonIcon />
+                    </NavIcon>
+                    <NavText style={{ fontFamily: 'Montserrat' }}>
+                        Contact
+                    </NavText>
+                </NavItem>
+            </SideNav.Nav>
+        </SideNav>
+    );
+};
+
+export default Sidebar;
