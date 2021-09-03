fcef4e3f06677c70da7120126f6bcb01bcd7c355
Robert McCurdy <rwmccurdy@hotmail.com>
2021-09-02 23:11:18 -0600

    Refined projects page, no more vid or slideshow

--- reactapp/src/components/Projects.js
+++ reactapp/src/components/Projects.js
@@ -1,24 +1,6 @@
-import ReactPlayer from 'react-player';
-import { Slide } from 'react-slideshow-image';
 import GitHubIcon from '@material-ui/icons/GitHub';
 import WebsiteIcon from '@material-ui/icons/Language';
-
-import 'react-slideshow-image/dist/styles.css';
-
-const slideImages = [
-    'https://github.com/RMcCurdy/Bandwagon/raw/main/frontend/img/home-page-bandwagon.png',
-    'https://github.com/RMcCurdy/Bandwagon/raw/main/frontend/img/landing-page-bandwagon.png',
-    'https://github.com/RMcCurdy/Bandwagon/raw/main/reactapp/public/img/landingpage/games-homepage.png',
-    'https://github.com/RMcCurdy/Bandwagon/raw/main/reactapp/public/img/landingpage/shop-homepage.png',
-    'https://github.com/RMcCurdy/Bandwagon/raw/main/reactapp/public/img/landingpage/badges-homepage.png',
-    'https://github.com/RMcCurdy/Bandwagon/raw/main/reactapp/public/img/landingpage/leaderboard-homepage.png',
-];
-
-const properties = {
-    duration: 3500,
-    transitionDuration: 800,
-    infinite: true,
-};
+import YouTubeIcon from '@material-ui/icons/YouTube';
 
 const Projects = () => {
     return (
@@ -52,64 +34,6 @@ const Projects = () => {
                         Database, and a React JS front end client.
                     </div>
 
-                    <div>
-                        <Slide easing='ease' {...properties}>
-                            <div className='each-slide'>
-                                <div
-                                    style={{
-                                        backgroundImage: `url(${slideImages[0]})`,
-                                        backgroundSize: '50vw',
-                                        backgroundRepeat: 'no-repeat',
-                                        backgroundPosition: 'center',
-                                    }}></div>
-                            </div>
-                            <div className='each-slide'>
-                                <div
-                                    style={{
-                                        backgroundImage: `url(${slideImages[1]})`,
-                                        backgroundSize: '50vw',
-                                        backgroundRepeat: 'no-repeat',
-                                        backgroundPosition: 'center',
-                                    }}></div>
-                            </div>
-                            <div className='each-slide'>
-                                <div
-                                    style={{
-                                        backgroundImage: `url(${slideImages[2]})`,
-                                        backgroundSize: '50vw',
-                                        backgroundRepeat: 'no-repeat',
-                                        backgroundPosition: 'center',
-                                    }}></div>
-                            </div>
-                            <div className='each-slide'>
-                                <div
-                                    style={{
-                                        backgroundImage: `url(${slideImages[3]})`,
-                                        backgroundSize: '35vw',
-                                        backgroundRepeat: 'no-repeat',
-                                        backgroundPosition: 'center',
-                                    }}></div>
-                            </div>
-                            <div className='each-slide'>
-                                <div
-                                    style={{
-                                        backgroundImage: `url(${slideImages[4]})`,
-                                        backgroundSize: '50vw',
-                                        backgroundRepeat: 'no-repeat',
-                                        backgroundPosition: 'center',
-                                    }}></div>
-                            </div>
-                            <div className='each-slide'>
-                                <div
-                                    style={{
-                                        backgroundImage: `url(${slideImages[5]})`,
-                                        backgroundSize: '45vw',
-                                        backgroundRepeat: 'no-repeat',
-                                        backgroundPosition: 'center',
-                                    }}></div>
-                            </div>
-                        </Slide>
-                    </div>
                     {/* https://github.com/RMcCurdy/Bandwagon */}
                 </div>
                 <div className='project-information-container project-bottom-margin'>
@@ -147,63 +71,93 @@ const Projects = () => {
                     <div className='project-information'>
                         A web-based application that provides students utilities
                         for tracking their school schedule and ordering takeout.
-                        Submission for CalgaryHacks 2021 Hackathon.
+                        Submission for CalgaryHacks 2021 Hackathon. This
+                        application uses JavaScript, HTML, CSS, Bootstrap, and
+                        MongoDB.
                     </div>
 
                     {/* https://duan-le.github.io/CalgaryHacks2021/ */}
                 </div>
-                <div
-                    className='project-information-container project-bottom-margin'
-                    style={{ display: 'flex' }}>
-                    <div style={{ width: '260%' }}>
-                        <div
-                            className='project-title'
-                            style={{ display: 'flex', alignItems: 'center' }}>
-                            WaitLess{' '}
-                            <GitHubIcon
-                                className='github-logo'
-                                onClick={() =>
-                                    window.open(
-                                        'https://github.com/RMcCurdy/TeamS_Project',
-                                        '_blank',
-                                    )
-                                }
-                                style={{
-                                    marginLeft: '1rem',
-                                    fontSize: '3rem',
-                                }}
-                            />
-                            <WebsiteIcon
-                                className='website-logo'
-                                onClick={() =>
-                                    window.open(
-                                        'https://edmunds13.github.io/#',
-                                        '_blank',
-                                    )
-                                }
-                                style={{
-                                    marginLeft: '1rem',
-                                    fontSize: '3.5rem',
-                                }}
-                            />
-                        </div>
-                        <div className='project-information'>
-                            A concept to improve the virtual academic advising
-                            experience for students by refining a virtual line
-                            up system.
-                        </div>
+                <div className='project-information-container project-bottom-margin'>
+                    <div
+                        className='project-title'
+                        style={{ display: 'flex', alignItems: 'center' }}>
+                        WaitLess{' '}
+                        <GitHubIcon
+                            className='github-logo'
+                            onClick={() =>
+                                window.open(
+                                    'https://github.com/RMcCurdy/TeamS_Project',
+                                    '_blank',
+                                )
+                            }
+                            style={{
+                                marginLeft: '1rem',
+                                fontSize: '3rem',
+                            }}
+                        />
+                        <WebsiteIcon
+                            className='website-logo'
+                            onClick={() =>
+                                window.open(
+                                    'https://edmunds13.github.io/#',
+                                    '_blank',
+                                )
+                            }
+                            style={{
+                                marginLeft: '1rem',
+                                fontSize: '3.5rem',
+                            }}
+                        />
+                        <YouTubeIcon
+                            className='website-logo'
+                            onClick={() =>
+                                window.open(
+                                    'https://www.youtube.com/watch?v=492zDqviTfQ',
+                                    '_blank',
+                                )
+                            }
+                            style={{
+                                marginLeft: '1rem',
+                                fontSize: '3.5rem',
+                            }}
+                        />
                     </div>
-
-                    <ReactPlayer
-                        url={
-                            'https://www.youtube.com/watch?v=492zDqviTfQ&t=15s'
-                        }
-                        playing
-                        muted
-                        loop
-                    />
-
-                    {/* https://edmunds13.github.io/# */}
+                    <div className='project-information'>
+                        A mobile application concept to improve the virtual
+                        academic advising experience for students by refining a
+                        virtual line up system. User Research, Flow Analysis,
+                        Affinity Diagrams, and a Cognitive Walkthrough were
+                        completed before designing both a Low-Fidelity prototype
+                        and a High-Fidelity prototype. Adobe XD was used to
+                        create the final demo.
+                    </div>
+                </div>
+                <div className='project-information-container project-bottom-margin'>
+                    <div
+                        className='project-title'
+                        style={{ display: 'flex', alignItems: 'center' }}>
+                        NFT Creation
+                        <WebsiteIcon
+                            className='website-logo'
+                            onClick={() =>
+                                window.open(
+                                    'https://opensea.io/Rajon',
+                                    '_blank',
+                                )
+                            }
+                            style={{
+                                marginLeft: '1rem',
+                                fontSize: '3.5rem',
+                            }}
+                        />
+                    </div>
+                    <div className='project-information'>
+                        A passion project to mint NFT's within the OpenSea
+                        peer-to-peer marketplace. My goal was to learn about the
+                        NFT space and do so by creating and selling my own
+                        NFT's.
+                    </div>
                 </div>
                 <div className='project-information-container'>
                     <div
@@ -226,7 +180,8 @@ const Projects = () => {
                     </div>
                     <div className='project-information'>
                         An AI that can perform a number of useful automated
-                        tasks and bot commands on a Discord server.
+                        tasks and bot commands on a Discord server. The Bot AI
+                        operates on JavaScript and runs off a local machine.
                     </div>
 
                     {/* https://github.com/RMcCurdy/Discord-Bot */}
