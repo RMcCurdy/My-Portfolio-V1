5868a5b02da65be33977ef7a2e4bea17b34909e5
Robert McCurdy <rwmccurdy@hotmail.com>
2021-09-05 22:52:06 -0600

    UI Fonts and Structure of AppJS

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -1,11 +1,12 @@
 import './styles/styles.css';
 import Logo from './img/mylogo1024.png';
-import Welcome from './components/Welcome.js';
-import Projects from './components/Projects.js';
-import Skills from './components/Skills.js';
-import About from './components/About.js';
-import Contact from './components/Contact.js';
-import Sidebar from './components/Sidebar.js';
+import Welcome from './components/Welcome';
+import Projects from './components/Projects';
+import Skills from './components/Skills';
+import About from './components/About';
+import Contact from './components/Contact';
+import Sidebar from './components/Sidebar';
+import Footer from './components/Footer';
 
 function App() {
     return (
@@ -26,15 +27,7 @@ function App() {
                 <Contact />
             </div>
 
-            <div className='portfolio-footer-container'>
-                <footer>
-                    {' '}
-                    <small>
-                        Copyright &copy; 2021 | Robert McCurdy | All Rights
-                        Reserved
-                    </small>{' '}
-                </footer>
-            </div>
+            <Footer />
         </>
     );
 }
---
+++ reactapp/src/components/Footer.js
@@ -0,0 +1,14 @@
+const Footer = () => {
+    return (
+        <div className='portfolio-footer-container'>
+            <footer>
+                {' '}
+                <small>
+                    Copyright &copy; 2021 | Robert McCurdy | All Rights Reserved
+                </small>{' '}
+            </footer>
+        </div>
+    );
+};
+
+export default Footer;
