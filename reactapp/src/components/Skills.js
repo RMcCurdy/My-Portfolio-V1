da5af9f3b917388d843534c44bab168f40981d4f
Robert McCurdy <rwmccurdy@hotmail.com>
2021-09-05 22:45:40 -0600

    Setting up Skills page

--- reactapp/src/components/Skills.js
+++ reactapp/src/components/Skills.js
@@ -1,13 +1,29 @@
+import ReactRotatingText from 'react-rotating-text';
+
 const Skills = () => {
     return (
         <div id='skills' className='content-container-not-welcome'>
             <div className='skills-content content-height'>
                 {/* Skills content */}
                 <div className='content-html-tag-font'>&lt;skills&gt;</div>
-                <p>React.js</p>
+                <ReactRotatingText
+                    items={[
+                        'React',
+                        'JavaScript',
+                        'HTML',
+                        'CSS',
+                        'SQL',
+                        'Python',
+                        'Java',
+                        'LaTeX',
+                        'Graphic Design',
+                        'MS Office',
+                    ]}
+                />
+                {/* <p>React.js</p>
                 <p>JavaScript</p>
                 <p>HTML</p>
-                <p>CSS</p>
+                <p>CSS</p> */}
                 <div className='content-html-tag-font'>&lt;/skills&gt;</div>
             </div>
         </div>
